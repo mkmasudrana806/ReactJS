4af322dce0314349a39f6ca1e390d92222e7f147
mkmasudrana806 <mkmasudrana806@gmail.com>
2023-11-24 19:27:10 +0600

    event handler and re-render control

--- 7.State_LifeCycle_in_classComponent/Code/src/components/Clock.js
+++ 7.State_LifeCycle_in_classComponent/Code/src/components/Clock.js
@@ -9,7 +9,7 @@ class Clock extends React.Component {
   //   };
   // }
 
-  state = { date: new Date() };
+  state = { date: new Date(), locale: "bn-BD" };
   // called this method when UI is showed
   componentDidMount() {
     this.clockTimer = setInterval(() => this.tick(), 1000);
@@ -25,16 +25,45 @@ class Clock extends React.Component {
     this.setState({ date: new Date() });
   }
 
+  // event handler for clock button
+  handleClockButtonClick(parameter) {
+    this.setState({ locale: "en-US" });
+  }
 
-
+  // lifecycle hook to control render
+  shouldComponentUpdate(nextProps) {
+    const { change: currentChange } = this.props;
+    const { change: nextChange } = nextProps;
+    if (currentChange === nextChange) return false;
+    else return true;
+  }
   render() {
+    const { date, locale } = this.state;
     return (
-      <h1 className="heading">
-        <span className="text">
-          {this.state.date.toLocaleTimeString(this.props.locale)}
-        </span>
-      </h1>
+      <>
+        <h1 className="heading">
+          <span className="text">
+            {this.state.date.toLocaleTimeString(this.state.locale)}
+          </span>
+        </h1>
+        <button onClick={() => this.handleClockButtonClick("en-US")}>
+          click me EN Timer
+        </button>
+      </>
     );
   }
 }
 export default Clock;
+
+// // this keywords problem and solution: use bind() method
+// const javascript = {
+//   name: "javascript",
+//   libraries: ["javascript", "react", " node"],
+//   printLibraries: function () {
+//     this.libraries.forEach(
+//       function (library) {
+//         console.log(`${this.name} loves ${library}`);
+//       }.bind(this)
+//     );
+//   },
+// };
